aee04493f90da6db51ea027161e2cf16a8d8c71d
lesego molefe <120086530+lesegomolefe@users.noreply.github.com>
2023-06-30 09:11:36 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,198 +1,222 @@
-<?php
-include 'config.php';
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
-
-
-session_start();
-
-//$user_id = $_POST['user_id'];
-$user_id = $_SESSION['user_id'];
-
-if(!isset($user_id)){
-  header('location:index.php');  
-};
-
-if(isset($_GET['logout'])){
-   // unset($user_id);
-    session_destroy();
-    header('location:login.php'); 
-    
-  };
-if(isset($_POST['add_to_cart'])){
-    $product_name = $_POST['product_name'];
-    $product_price = $_POST['product_price'];
-    $product_image = $_POST['product_image'];
-    $product_quantity = $_POST['product_quantity'];
-
-    $select_cart = mysqli_query($conn, "SELECT * FROM 'cart' WHERE name = '$product_name'
-    AND user_id = '$user_id'") or die('query failed');
-    if(mysqli_num_rows($select_cart) > 0){
-        $message[] = 'product already added to cart!';
-    }else{
-        mysqli_query($conn, "INSERT INTO 'cart' (user_id, name, price, image, quantity) VALUES
-        ('user_id', '$product_name', '$product_price', '$product_image', '$product_quantity')")
-        or die('query failed');
-        $message[] = 'product already added to cart!';
-    }
-}
-
-if(isset($_POST['update_cart'])){
-    $update_quantity = $_POST['cart_quantity'];
-    $update_id = $_POST['cart_id'];
-    mysqli_query($conn, "UPDATE 'cart' SET quantity = '$update_quantity' WHERE id ='$update_id'") or die('query failed');
-    $message[] = 'cart  quantity updated successfully!';
-
-}
-
-if(isset($_GET['remove'])){
-    $remove_id = $_GET['remove'];
-    mysqli_query($conn, "DELETE FROM 'cart' WHERE id = '$remove_id'") or die('query failed');
-    header('location:index.php');
-}
-
-if(isset($_GET['delete_all'])){
-    mysqli_query($conn, "DELETE FROM 'cart' WHERE user_id = '$user_id'") or die('query failed');
-    header('location:index.php');
-
-}
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+	
+<?php session_start(); ?>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title><?php echo isset($_SESSION['system']['name']) ? $_SESSION['system']['name'] : '' ?></title>
+ 	
 
 <?php
-if(isset($message)){
-    foreach($message as $message){
-        echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
-    }
+ include('./header.php'); 
+ // include('./auth.php'); 
+ ?>
+
+</head>
+<style>
+	body{
+        background: #80808045;
+  }
+  .modal-dialog.large {
+    width: 80% !important;
+    max-width: unset;
+  }
+  .modal-dialog.mid-large {
+    width: 50% !important;
+    max-width: unset;
+  }
+  #viewer_modal .btn-close {
+    position: absolute;
+    z-index: 999999;
+    /*right: -4.5em;*/
+    background: unset;
+    color: white;
+    border: unset;
+    font-size: 27px;
+    top: 0;
 }
-
-?>
-<div class="container">
-    <div class="user-profile">
-        <?php
-        $select_user =  "SELECT * FROM 'user_form' WHERE id= '$user_id'
-        or die('query failed')";
-        if($result=mysqli_query($conn,$select_user)){
-            $row = mysqli_num_rows($result);
-            $fetch_user = mysqli_fetch_assoc($select_user);
-        }
-          
-
-        ?>
-        <p>username : <span><?php echo $fetch_user['name']; ?></span></p>
-        <p>email : <span> <?php echo $fetch_user['email']; ?></span></p>
-        <div class="flex">
-            <a href="login.php" class="btn">login</a>
-            <a href="register.php" class="option-btn">register</a>
-            <a href="index.php?logout=<?php echo $user_id; ?>" onclick=" return confirm('are your sure you want to logout?');" 
-            class="delete-btn">Logout</a>
-
-        </div>
+#viewer_modal .modal-dialog {
+        width: 80%;
+    max-width: unset;
+    height: calc(90%);
+    max-height: unset;
+}
+  #viewer_modal .modal-content {
+       background: black;
+    border: unset;
+    height: calc(100%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #viewer_modal img,#viewer_modal video{
+    max-height: calc(100%);
+    max-width: calc(100%);
+  }
 
 
+span.select2-selection.select2-selection--single {
+    min-height: 2rem;
+  }
+</style>
+
+<body>
+	<?php include 'topbar.php' ?>
+  <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-body text-white">
     </div>
-    <div class="products">
+  </div>
+  
+  <main id="view-panel" >
+      <?php 
+      $page = isset($_GET['page']) ? $_GET['page'] :'home'; 
+      ?>
+  	<?php include $page.'.php' ?>
+  </main>
 
-    <h1 class="heading">latest products</h1>
-        <div class="box-container">
-        <?php
-         $select_product = mysqli_query($conn, "SELECT * FROM 'products'")
-         or die('query failed');
-         if(mysqli_num_rows($select_product) > 0){
-           while( $fetch_product = mysqli_fetch_assoc($select_product)){
-         ?>
-         <form action="" method="post" class="box">
-            <img src="cam.jpg/<?php echo $fetch_product['image']  ?>" alt="">
-            <div class="name"><?php echo $fetch_product['name'];?></div>
-            <div class="price"><?php echo $fetch_product['price'];?></div>
-            <input type="number" min="1" name="product_quqntity" value="1">
-            <input type="hidden" name="product_image" value="<?php echo $fetch_product['image'];?>">
-            <input type="hidden" name="product_name" value="<?php echo $fetch_product['name'];?>">
-            <input type="hidden" name="product_price" value="<?php echo $fetch_product['price'];?>">
-            <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
-         </form>
-         <?php
-         };
-        };
-
-        ?>
-        </div>
-
+<div class="modal fade" id="confirm_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title">Confirmation</h5>
+      </div>
+      <div class="modal-body">
+        <div id="delete_content"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      </div>
     </div>
-    <div class="shoppin-cart">
-        <h1 class="heading">shopping cart</h1>
-        <table>
-            <thead>
-                <th>image</th>
-                <th>name</th>
-                <th>price</th>
-                <th>quantity</th>
-                <th>total price</th>
-                <th>action</th>
-            </thead>
-            <tbody>
-                <?php
-                $select_product = mysqli_query($conn, "SELECT * FROM 'cart' 
-                WHERE user_id = '$user_id'") or die('query failed');
-                if(mysqli_num_rows($cart_query) > 0){
-                    while($fetch_cart = mysqli_fetch_assoc($cart_query)){
-                ?> 
-                <tr>
-                    <td><img src="cam <?php echo $fetch_cart['image']; ?>" height="100" alt=""></td>
-                    <td><?php echo $fetch_cart['name']; ?></td>
-                    <td><?php echo $fetch_cart['price']; ?></td>
-                    <td>
-                        <form action="" method="post">
-                            <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>" >
-                            <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>" >
-                            <input type="submit" name="update_cart" value="update" class="option-btn">
-                        </form>
-                    </td>
-                    <td>$<?php echo $sub_total = number_format($fetch_cart['price'] * $fetch_cart['quantity']);?>/-</td>
-                    <td><a href="index.php?remove=<?php echo $fetch_cart['id'];?>"
-                    class="delete-btn" onclick="return confirm('remove item from acrt?');">remove</a></td>
-                </tr>
-
-                <?php
-                $grand_total += $sub_total;
-                    };
-                }else{
-                    echo '<tr><td style="padding:20px; text-transform:capitalize;" colspan="6"> no item added </td></tr>';
-                }
-
-                ?>
-                <tr class="table-bottom">
-                    <td colspan="4">grand total :</td>
-                    <td>$<?php $grand_total; ?>/-</td>
-                    <td><a href="index.php?delete_all" onclick="return confirm('delete all from cart?');"
-                    class="delete-btn"<?php echo ($grand_total > 1)? '':'disabled';?>>delete all</a</td>
-
-                </tr>
-
-         </tbody>
-
-        </table>
-        <div class="cart-btn">
-            <a href="#" class="btn <?php echo ($grand_total > 1)? '':'disabled';?>">Proceed to checkout</a>
-
-        </div>
-
+  </div>
+  <div class="modal fade" id="uni_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title"></h5>
+      </div>
+      <div class="modal-body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+      </div>
+      </div>
     </div>
-
-
-</div>
-    
+  </div>
+  <div class="modal fade" id="viewer_modal" role='dialog'>
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+              <button type="button" class="btn-close" data-dismiss="modal"><span class="fa fa-times"></span></button>
+              <img src="" alt="">
+      </div>
+    </div>
+  </div>
 </body>
+<script>
+	 window.start_load = function(){
+    $('body').prepend('<di id="preloader2"></di>')
+  }
+  window.end_load = function(){
+    $('#preloader2').fadeOut('fast', function() {
+        $(this).remove();
+      })
+  }
+ window.viewer_modal = function($src = ''){
+    start_load()
+    var t = $src.split('.')
+    t = t[1]
+    if(t =='mp4'){
+      var view = $("<video src='"+$src+"' controls autoplay></video>")
+    }else{
+      var view = $("<img src='"+$src+"' />")
+    }
+    $('#viewer_modal .modal-content video,#viewer_modal .modal-content img').remove()
+    $('#viewer_modal .modal-content').append(view)
+    $('#viewer_modal').modal({
+            show:true,
+            backdrop:'static',
+            keyboard:false,
+            focus:true
+          })
+          end_load()  
+
+}
+  window.uni_modal = function($title = '' , $url='',$size=""){
+      start_load()
+      $.ajax({
+          url:$url,
+          error:err=>{
+              console.log()
+              alert("An error occured")
+          },
+          success:function(resp){
+              if(resp){
+                  $('#uni_modal .modal-title').html($title)
+                  $('#uni_modal .modal-body').html(resp)
+                  if($size != ''){
+                      $('#uni_modal .modal-dialog').addClass($size)
+                  }else{
+                      $('#uni_modal .modal-dialog').removeAttr("class").addClass("modal-dialog modal-md")
+                  }
+                  $('#uni_modal').modal({
+                    show:true,
+                    backdrop:'static',
+                    keyboard:false,
+                    focus:true
+                  })
+                  end_load()
+              }
+          }
+      })
+  }
+  window._conf = function($msg='',$func='',$params = []){
+     $('#confirm_modal #confirm').attr('onclick',$func+"("+$params.join(',')+")")
+     $('#confirm_modal .modal-body').html($msg)
+     $('#confirm_modal').modal('show')
+  }
+   window.alert_toast= function($msg = 'TEST',$bg = 'success'){
+      $('#alert_toast').removeClass('bg-success')
+      $('#alert_toast').removeClass('bg-danger')
+      $('#alert_toast').removeClass('bg-info')
+      $('#alert_toast').removeClass('bg-warning')
+
+    if($bg == 'success')
+      $('#alert_toast').addClass('bg-success')
+    if($bg == 'danger')
+      $('#alert_toast').addClass('bg-danger')
+    if($bg == 'info')
+      $('#alert_toast').addClass('bg-info')
+    if($bg == 'warning')
+      $('#alert_toast').addClass('bg-warning')
+    $('#alert_toast .toast-body').html($msg)
+    $('#alert_toast').toast({delay:3000}).toast('show');
+  }
+  $(document).ready(function(){
+    $('#preloader').fadeOut('fast', function() {
+        $(this).remove();
+      })
+  })
+
+  $('.datetimepicker').datetimepicker({
+      format:'Y/m/d H:i',
+      startDate: '+3d'
+  })
+
+  $('.select2').select2({
+    placeholder:"Please select here",
+    width: "100%"
+  })
+  $('.number').on('input',function(){
+        var val = $(this).val()
+        val = val.replace(/[^0-9 \,]/, '');
+        $(this).val(val)
+    })
+</script>	
 </html>
--- login.php
+++ login.php
@@ -1,61 +1,54 @@
-<?php
+<?php include 'db_connect.php' ?>
+<div class="container-fluid">
+	<form action="" id="login-frm">
+		<div class="form-group">
+			<label for="" class="control-label">Email</label>
+			<input type="email" name="email" required="" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="" class="control-label">Password</label>
+			<input type="password" name="password" required="" class="form-control">
+			<small><a href="index.php?page=signup" id="new_account">Create New Account</a></small>
+		</div>
+		<span class="float-right">
+			<button class="button btn btn-primary btn-sm">Login</button>
+			<button class="button btn btn-secondary btn-sm" type="button" data-dismiss="modal">Cancel</button>
+		</span>
+	</form>
+</div>
+<style>
+	#uni_modal .modal-footer{
+		display: none
+	}
+</style>
+<script>
+	$('#login-frm').submit(function(e){
+		e.preventDefault()
+		start_load()
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'ajax.php?action=login',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
 
-include 'config.php';
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
-if(isset($_POST['submit'])){
-  //  $name =  $_POST['name'];
-    $email =  $_POST['email'];
-    $password  =  $_POST['password'];
-  //  $cpassword =  $_POST['cpassword'];
-
-    $sql = "SELECT * FROM 'user_form' WHERE email = '$email' AND password = '$password' or die ('query failed')";
-     if($result=mysqli_query($conn,$sql)){
-      $row=  mysqli_num_rows($result);
-;
-        $row = mysqli_fetch_assoc($result);
-        $_SESSION['user_id'] = $row['id'];
-        header('location:index.php');
-      
-    
-     }else{
-          $message[] = 'incorrect password or email';
-     }
-}
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<?php
-if(isset($message)){
-    foreach($message as $message){
-        echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
-    }
-}
-
-?>
-    <div class="form-container">
-        <form action="" method="post">
-            <h3>login now</h3>
-          
-            <input type="email" name="email" required placeholder="enter email" class="box">
-            <input type="password" name="password" required placeholder="enter password" class="box">
-           
-            <input type="submit" name="submit"  class="btn" value="login now">
-            <p>don't have an account? <a href="register.php">Register now</a></p>
-        </form>
-
-    </div>
-    
-</body>
-</html>
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='index.php?page=home';
+				}else{
+					$('#login-frm').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+					end_load()
+				}
+			}
+		})
+	})
+	$('.number').on('input',function(){
+        var val = $(this).val()
+        val = val.replace(/[^0-9 \,]/, '');
+        $(this).val(val)
+    })
+</script>	
